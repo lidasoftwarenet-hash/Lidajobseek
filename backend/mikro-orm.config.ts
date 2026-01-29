fb5e4bb5a08124bd3632b505f2d354210742e978
Hen <bandel79@gmail.com>
2026-01-29 19:50:01 +0200

    newnpm

--- backend/mikro-orm.config.ts
+++ backend/mikro-orm.config.ts
@@ -10,21 +10,13 @@ import { SelfReview } from './src/reviews/self-review.entity';
 
 const config: MikroOrmModuleOptions = {
   driver: PostgreSqlDriver,
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  dbName: process.env.DB_NAME || 'lidajobseek',
+  clientUrl: process.env.DATABASE_URL,
   entities: [User, Process, Interaction, Contact, Resource, SelfReview],
   metadataProvider: TsMorphMetadataProvider,
   allowGlobalContext: true,
   discovery: {
     warnWhenNoEntities: false,
   },
-  schemaGenerator: {
-    disableForeignKeys: false,
-  },
-  schema: 'app',
 };
 
 export default config;
